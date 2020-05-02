a72cdbce93d438700e93810688038bf67f5a0a41
Junyi <qsong.vip@qq.com>
2020-05-02 18:20:17 +0800

    [Junyi] Delete unused import statement.

--- src/main/java/com/newroadedu/erp/nwerp/dao/UserRepository.java
+++ src/main/java/com/newroadedu/erp/nwerp/dao/UserRepository.java
@@ -4,8 +4,7 @@ import com.newroadedu.erp.nwerp.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Date;
-import java.util.List;
-
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> { }
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
